9b59cb4e423de361e8f7fa734b032e02cc91b932
Aashir Nawaz <aashir12>
2026-09-01 14:15:02 +0500

    removed class from logo

--- src/lib/pdf/document-letter-template.tsx
+++ src/lib/pdf/document-letter-template.tsx
@@ -163,7 +163,7 @@ export const DocumentLetterTemplate = ({
       <View style={styles.headerRow}>
         <Text style={styles.addressLine}>{SIDHU_COMPANY.addressLine}</Text>
         <View style={styles.logoColumn}>
-          <Image src={logoSrc} style={styles.logo} />
+          <Image src={logoSrc} />
         </View>
       </View>
 
